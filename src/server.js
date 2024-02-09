1c6ad679b7d4bacac57d8020dad031260d8b6be2
E-Han Song <iansong0714@gmail.com>
2024-02-09 22:06:02 +0900

    WebSocket and Implement Messages
    - Learned about the concepts of http and WebSocket
    - Diff. bet. socket in app.js & server.js
    - Implement message function
    - Learned how to distinguish nicknames and messages using JSON.stringify and JSON.parse

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import http from "http";
+import {WebSocketServer} from "ws";
+import express from "express";
+
+const app = express();
+const sockets = [];
+
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+app.use("/public", express.static(__dirname+"/public"));
+
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening`);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anonymous";
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("Disconnected from the Browser"));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+        
+    });
+});
+
+server.listen(3000, handleListen);
