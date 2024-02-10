42a20ea082a25db269c32f08dc411e6e5790e815
E-Han Song <iansong0714@gmail.com>
2024-02-10 18:15:08 +0900

    Messengers
    - Learned what is socketIO
    - Learned how to communicate realtime bet. server&browser
    - Implemented Basic Messenger Functions
    - Code Chal: Implement User name configuration form and change name form(Completed)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,40 +1,65 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload){
-    const msg = {type, payload};
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Server")
-});
+room.hidden = true;
+let roomName;
+let nickName;
 
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-    console.log("Disconnected")
-});
-
-function handleSubmit (event) {
+function handleMessageSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
     input.value = "";
 };
 
-function handleNickSubmit (event) {
+function handleChangeNameSubmit(event){
     event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
+    const input = room.querySelector("#changeName input");
+    const value = input.value;
+    nickName = value;
+    socket.emit("new_name", value);
     input.value = "";
 }
 
-nickForm.addEventListener("submit", handleNickSubmit);
-messageForm.addEventListener("submit", handleSubmit);
+function showRoom(){
+    room.hidden = false;
+    welcome.hidden = true;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const changeNameForm = room.querySelector("#changeName");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    changeNameForm.addEventListener("submit", handleChangeNameSubmit);
+}
+
+function handleRoomSubmit (event) {
+    event.preventDefault();
+    const newroomName = form.querySelector("#roomName");
+    const nickname = form.querySelector("#nickName");
+    socket.emit("enter_room", newroomName.value, nickname.value, showRoom);
+    roomName = newroomName.value;
+    nickName = nickname.value;
+    const changeName = room.querySelector("#changeName");
+    changeName.value = nickname.value;
+    newroomName.value = "";
+    nickname.value = "";
+};
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => addMessage(`${user} joined. Welcome!`));
+socket.on("bye", (user) => addMessage(`${user} has left the chat`));
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 import http from "http";
-import {WebSocketServer} from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
-const sockets = [];
 
 app.set("view engine", "pug");
 app.set("views", __dirname+"/views");
@@ -11,26 +10,27 @@ app.use("/public", express.static(__dirname+"/public"));
 
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
-const handleListen = () => console.log(`Listening`);
 
-const server = http.createServer(app);
-const wss = new WebSocketServer({server});
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anonymous";
-    console.log("Connected to Browser");
-    socket.on("close", () => console.log("Disconnected from the Browser"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type){
-            case "new_message":
-                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-        
+wsServer.on("connection", socket => {
+    socket["nickname"] = "Anon";
+    socket.on("enter_room", (roomName, nickName, done) => {
+        socket["nickname"] = nickName;
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname);
     });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+    });
+    socket.on("new_message", (message, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${message}`);
+        done();
+    });
+    socket.on("new_name", (nickName) => socket["nickname"] = nickName);
 });
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening`);
+httpServer.listen(3000, handleListen);
