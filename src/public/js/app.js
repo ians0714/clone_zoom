1b586c613a1917718ec4f8dd9b805cee82a0b113
E-Han Song <iansong0714@gmail.com>
2024-02-12 19:54:39 +0900

    Web RTC connection(offer)
    - Learned how offers create and sent(create peer connection, give data, make an offer, set local descriptions)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,8 @@
 const socket = io();
 
+const welcome = document.getElementById("welcome");
+const enterForm = welcome.querySelector("form");
+const call = document.getElementById("call");
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const camBtn = document.getElementById("camera");
@@ -8,26 +11,41 @@ const camSelect = document.getElementById("cameras");
 let myStream; // Stream
 let muted = false; // Audio State
 let camOff = false; // Camera State
+let roomName; // Accessed Room Name
+let myPeerConnection; // Peer Connection of the browser
+welcome.hidden = false; // Open welcome part
+call.hidden = true; // hide call part
 
 async function getCameras(){
     const devices = navigator.mediaDevices.enumerateDevices(); // Get array of all devices
     const cameras = devices.filter((device) => device.kind === "videoinput"); // Filter and get cam devices
+    const curCam = myStream.getVideoTracks()[0]; // Get current camera track
     cameras.forEach((cam) => {
         const option = document.createElement("option");
         option.value = cam.deviceId;
         option.innerText = cam.label;
+        if(curCam.label === cam.label){
+            option.selected = true; // Select current camera
+        }
         camSelect.appendChild(option);
     }); // Append all cam devices into selection
 };
 
-async function getMedia(){
+async function getMedia(deviceId){
+    const initConst = {
+        audio: true, video: { facingMode: "user" },
+    }; // Default device
+    const camConst = {
+        audio: true, video: { deviceId: { exact: deviceId } },
+    } // Camera we want to change with
     try{
-        myStream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true,
-        }); // Try to get videos
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId? camConst : initConst
+        ); // Try to get videos
         myFace.srcObject = myStream;
-        await getCameras();
+        if(!deviceId){
+            await getCameras();
+        } // Only runs function at the first time
     } catch (error) {
         console.log(error); // If error has caught, print error log on the console
     }
@@ -59,8 +77,47 @@ function handleCameraClick() { // Change cam state
     }
 };
 
-getMedia();
+async function handleCameraChange() {
+    await getMedia(camSelect.value);
+} // Change camera
+
+async function startMedia(){
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection(); // Make connection
+}; // Hide welcome and paint call
+
+function handleRoomEnter(event) {
+    event.preventDefault();
+    const input = welcome.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}; // Send input value and enter the room
 
 // Event Listeners
 muteBtn.addEventListener("click", handleMuteClick);
 camBtn.addEventListener("click", handleCameraClick);
+enterForm.addEventListener("submit", handleRoomEnter);
+camSelect.addEventListener("input", handleCameraChange);
+
+// Socket Code
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit("offer", offer, roomName);
+}); // Create offer and set local description then emit the offer
+
+socket.on("offer", offer => {
+    console.log("sent offer");
+});
+
+// RTC Code
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection(); // Create Peer Connection
+    myStream.getTracks().forEach(track => {
+        myPeerConnection.addtrack(track, myStream);
+    }); // Add tracks to send datas of the stream to the connection
+
+}
